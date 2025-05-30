98e30e270145f65f41e9e3c26a286b626011a2fb
Fernando B Oliveira <fboliveira25@gmail.com>
2025-05-30 15:39:03 -0300

    added: user converter

--- Codes/ticket/user/src/main/java/br/ufop/edu/web/ticket/user/controllers/UserController.java
+++ Codes/ticket/user/src/main/java/br/ufop/edu/web/ticket/user/controllers/UserController.java
@@ -1,15 +1,33 @@
 package br.ufop.edu.web.ticket.user.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ufop.edu.web.ticket.user.dtos.SimpleUserRecordDTO;
+import br.ufop.edu.web.ticket.user.service.UserService;
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class UserController {
+
+    private final UserService userService;
     
     @GetMapping("/users/status")
     public ResponseEntity<String> getStatus() {
         return ResponseEntity.ok("Service is running");
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<SimpleUserRecordDTO>> getAllUsers() {
+
+        List<SimpleUserRecordDTO> list =
+            userService.getAllUsers();
+        return ResponseEntity.ok(list);
+
     }
 
 }
