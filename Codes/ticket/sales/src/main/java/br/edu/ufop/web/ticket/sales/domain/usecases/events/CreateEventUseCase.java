088b8beb3e488164f201956df1075ee649c40efd
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-08 18:51:59 -0300

    updated event type validate

--- Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/domain/usecases/events/CreateEventUseCase.java
+++ Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/domain/usecases/events/CreateEventUseCase.java
@@ -1,21 +1,21 @@
 package br.edu.ufop.web.ticket.sales.domain.usecases.events;
 
-import br.edu.ufop.web.ticket.sales.dtos.events.CreateEventDTO;
+import br.edu.ufop.web.ticket.sales.domain.EventDomain;
 import br.edu.ufop.web.ticket.sales.enums.EnumEventType;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class CreateEventUseCase {
     
-    CreateEventDTO createEventDTO;
+    EventDomain eventDomain;
 
     public void validate() {
         validateType();
     }
 
     private void validateType() {
-        if (createEventDTO.getType() == null || createEventDTO.getType() >= EnumEventType.values().length) {
-            createEventDTO.setType(EnumEventType.EVENTO.getId());
+        if (eventDomain.getType() == null) {
+            eventDomain.setType(EnumEventType.EVENTO);
         }
     }
 
