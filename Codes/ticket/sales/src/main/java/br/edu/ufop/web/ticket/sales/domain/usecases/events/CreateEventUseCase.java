374e037cbcf9010133c590394e89bb4bedbf9126
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-08 19:24:27 -0300

    Updated annotation

--- Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/domain/usecases/events/CreateEventUseCase.java
+++ Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/domain/usecases/events/CreateEventUseCase.java
@@ -2,9 +2,11 @@ package br.edu.ufop.web.ticket.sales.domain.usecases.events;
 
 import br.edu.ufop.web.ticket.sales.domain.EventDomain;
 import br.edu.ufop.web.ticket.sales.enums.EnumEventType;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class CreateEventUseCase {
     
     EventDomain eventDomain;
