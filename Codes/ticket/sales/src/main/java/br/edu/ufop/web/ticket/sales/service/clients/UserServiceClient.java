033b1bfe2512d818ae71db93d720d638bdfce7d4
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-08 21:22:34 -0300

    updated: interface users openfeign client

--- Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/service/clients/UserServiceClient.java
+++ Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/service/clients/UserServiceClient.java
@@ -1,5 +1,17 @@
 package br.edu.ufop.web.ticket.sales.service.clients;
 
-public class UserServiceClient {
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.edu.ufop.web.ticket.sales.dtos.external.users.UserDTO;
+
+@FeignClient("users-service")
+public interface UserServiceClient {
+
+    @GetMapping("/users")
+    List<UserDTO> getAllUsers();
     
 }
