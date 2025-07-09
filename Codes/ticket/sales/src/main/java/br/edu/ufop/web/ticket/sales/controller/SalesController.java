2472d6fd8962840a33ac79cf549c176fcbb94c85
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-08 21:22:08 -0300

    updated: request from users client

--- Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/controller/SalesController.java
+++ Codes/ticket/sales/src/main/java/br/edu/ufop/web/ticket/sales/controller/SalesController.java
@@ -2,18 +2,32 @@ package br.edu.ufop.web.ticket.sales.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ufop.web.ticket.sales.dtos.external.users.UserDTO;
+import br.edu.ufop.web.ticket.sales.service.clients.UserServiceClient;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 @RequestMapping("/sales")
+@AllArgsConstructor
 public class SalesController {
     
+    private final UserServiceClient userServiceClient;
+
     @GetMapping("/status")
     public ResponseEntity<String> status() {
         return ResponseEntity.ok("Sales service is running with livereload.");
     }
-    
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return ResponseEntity.ok(userServiceClient.getAllUsers());
+    }
 
 }
