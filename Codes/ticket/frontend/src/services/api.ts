4c3399f2b87b46d7ca19658fb6e2b79a89254f32
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-17 19:39:18 -0300

    updated endpoint log message

--- Codes/ticket/frontend/src/services/api.ts
+++ Codes/ticket/frontend/src/services/api.ts
@@ -2,7 +2,7 @@ const URL = 'http://localhost:8080'
 
 const api = async (endpoint : string, config? : RequestInit | undefined) => {
 
-    console.log(URL + endpoint)
+    console.log(`Endpoint: ${URL}${endpoint}`)
     const result = await fetch(URL + endpoint, config)
     return await result.json()
 
