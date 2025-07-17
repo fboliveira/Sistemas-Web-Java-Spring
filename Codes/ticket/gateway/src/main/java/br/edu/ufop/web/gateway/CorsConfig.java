da1c3adf37953786f364ecc8caf1b48e1287a9ac
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-16 22:42:53 -0300

    updated: cors mapping

--- Codes/ticket/gateway/src/main/java/br/edu/ufop/web/gateway/CorsConfig.java
+++ Codes/ticket/gateway/src/main/java/br/edu/ufop/web/gateway/CorsConfig.java
@@ -1,26 +1,18 @@
 package br.edu.ufop.web.gateway;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 // https://docs.spring.io/spring-framework/reference/web/webmvc-cors.html
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+@EnableWebFlux
+public class CorsConfig implements WebFluxConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-
-		registry.addMapping("*") // /api/**
-			.allowedOrigins("*")
-			.allowedMethods("*")
-			.allowedHeaders("*")
-			.exposedHeaders("*")
-			.allowCredentials(true).maxAge(3600);
-
+		registry.addMapping("/**");		
 	}
 }
