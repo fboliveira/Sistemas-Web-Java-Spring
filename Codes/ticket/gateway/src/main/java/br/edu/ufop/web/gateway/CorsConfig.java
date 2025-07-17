3c4a32b72e5fabb5f5f5f83c51efc697edf93fd2
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-17 19:39:44 -0300

    fixed: cors config

--- Codes/ticket/gateway/src/main/java/br/edu/ufop/web/gateway/CorsConfig.java
+++ Codes/ticket/gateway/src/main/java/br/edu/ufop/web/gateway/CorsConfig.java
@@ -1,18 +1,43 @@
 package br.edu.ufop.web.gateway;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 // https://docs.spring.io/spring-framework/reference/web/webmvc-cors.html
+// https://docs.spring.io/spring-framework/reference/web/webflux-cors.html#webflux-cors-webfilter
 
 @Configuration
-@EnableWebFlux
-public class CorsConfig implements WebFluxConfigurer {
+// @EnableWebFlux
+public class CorsConfig { // implements WebFluxConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");		
-	}
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**");
+    // }
+
+    @Bean
+    CorsWebFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Possibly...
+        // config.applyPermitDefaultValues()
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5555");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
 }
