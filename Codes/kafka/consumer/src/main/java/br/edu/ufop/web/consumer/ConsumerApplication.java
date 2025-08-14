441d85a44b80630d07d1bc314469a7574cd37081
Fernando B Oliveira <fboliveira25@gmail.com>
2025-08-13 22:10:38 -0300

    updated: consumer - exclude database config

--- Codes/kafka/consumer/src/main/java/br/edu/ufop/web/consumer/ConsumerApplication.java
+++ Codes/kafka/consumer/src/main/java/br/edu/ufop/web/consumer/ConsumerApplication.java
@@ -2,8 +2,9 @@ package br.edu.ufop.web.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
