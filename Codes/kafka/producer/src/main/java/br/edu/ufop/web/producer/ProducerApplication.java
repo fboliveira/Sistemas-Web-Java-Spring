0fad2714a0777da73f7afdf15bfe7e3a0d793824
Fernando B Oliveira <fboliveira25@gmail.com>
2025-08-13 22:12:57 -0300

    updated: producer - exclude database config

--- Codes/kafka/producer/src/main/java/br/edu/ufop/web/producer/ProducerApplication.java
+++ Codes/kafka/producer/src/main/java/br/edu/ufop/web/producer/ProducerApplication.java
@@ -2,8 +2,9 @@ package br.edu.ufop.web.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ProducerApplication {
 
 	public static void main(String[] args) {
