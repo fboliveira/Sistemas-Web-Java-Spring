4de4b8fb78319e950f1c5abe9eda38da29e82e0e
Fernando B Oliveira <fboliveira25@gmail.com>
2025-10-22 20:29:43 -0300

    added: credit card network domain

---
+++ Codes/tickets/users/src/main/java/br/edu/ufop/web/users/domain/CCNetworkDomain.java
@@ -0,0 +1,17 @@
+package br.edu.ufop.web.users.domain;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CCNetworkDomain {
+
+    private UUID id;
+    private String name;
+    
+}
