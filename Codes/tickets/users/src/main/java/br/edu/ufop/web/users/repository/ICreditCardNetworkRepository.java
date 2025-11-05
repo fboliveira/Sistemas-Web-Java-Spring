4c3802a6cce98e44ddec7cb94ab1a926fb7dc9a2
Fernando B Oliveira <fboliveira25@gmail.com>
2025-11-05 19:46:45 -0300

    updated: new queries

--- Codes/tickets/users/src/main/java/br/edu/ufop/web/users/repository/ICreditCardNetworkRepository.java
+++ Codes/tickets/users/src/main/java/br/edu/ufop/web/users/repository/ICreditCardNetworkRepository.java
@@ -2,8 +2,11 @@ package br.edu.ufop.web.users.repository;
 
 import br.edu.ufop.web.users.entity.CCNetworkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ICreditCardNetworkRepository extends JpaRepository<CCNetworkEntity, UUID> {
@@ -11,6 +14,19 @@ public interface ICreditCardNetworkRepository extends JpaRepository<CCNetworkEnt
 
     List<CCNetworkEntity> findByName(String name);
     List<CCNetworkEntity> findByNameContainingIgnoreCase(String name);
+
+    List<CCNetworkEntity> findByComment(String comment);
+
+    List<CCNetworkEntity> findByTaxBefore(Double taxBefore);
+
+    @Query(value = "SELECT cce FROM CCNetworkEntity cce " +
+            "WHERE cce.tax = :tax")
+    Optional<CCNetworkEntity> procurarPorTaxa(@Param("tax") Double tax);
+
+    @Query(value = "select * from tb_credit_card_network " +
+            " where tax = :tax ", nativeQuery = true)
+    Optional<CCNetworkEntity> procurarPorTaxaSQLNativo(@Param("tax") Double tax);
+
 
 
 
