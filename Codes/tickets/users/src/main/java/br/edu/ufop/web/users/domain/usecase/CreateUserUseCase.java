fe51a38314eaffe456bb7e192fdf92bdb7b5481d
Fernando B Oliveira <fboliveira25@gmail.com>
2025-11-19 17:08:19 -0300

    updated: validate ccn id using repository adapter

--- Codes/tickets/users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
+++ Codes/tickets/users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
@@ -1,18 +1,28 @@
 package br.edu.ufop.web.users.domain.usecase;
 
+import br.edu.ufop.web.users.exception.CCNetworkNotFoundException;
+import br.edu.ufop.web.users.domain.CCNetworkDomain;
 import br.edu.ufop.web.users.domain.UserDomain;
-import lombok.AllArgsConstructor;
+import br.edu.ufop.web.users.domain.port.CCNRepositoryPort;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
+import java.util.Optional;
+import java.util.UUID;
+
 @RequiredArgsConstructor
+@Component
 public class CreateUserUseCase {
 
+    @Setter
     private UserDomain userDomain;
+    private final CCNRepositoryPort repositoryPort;
 
     public void validate() {
         // Validações conforme a regra de negócio para a criação do usuário
         validateName();
+        validateCCNetwork();
 
         // Demais validações
 
@@ -22,6 +32,22 @@ public class CreateUserUseCase {
         if (this.userDomain.getName() == null) {
             throw new RuntimeException("Name is null");
         }
+    }
+
+    private void validateCCNetwork() {
+
+        if (this.userDomain.getCcNetwork() == null) {
+            throw new RuntimeException("Credit Card Network is null");
+        }
+
+        UUID id = this.userDomain.getCcNetwork().getId();
+        Optional<CCNetworkDomain> optional = repositoryPort.findById(id);
+
+        if (optional.isEmpty()) {
+            throw new CCNetworkNotFoundException("Credit Card Network does not found.");
+        }
+
+
     }
 
 
