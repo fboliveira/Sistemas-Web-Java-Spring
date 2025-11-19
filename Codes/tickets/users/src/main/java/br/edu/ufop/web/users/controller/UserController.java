8f5d2bd9c0172e41df0dbf3733ca6f1a166f16c1
Fernando B Oliveira <fboliveira25@gmail.com>
2025-11-19 17:07:42 -0300

    updated: get all and save

--- Codes/tickets/users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
+++ Codes/tickets/users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
@@ -1,22 +1,35 @@
 package br.edu.ufop.web.users.controller;
 
+import br.edu.ufop.web.users.dto.CreateUserDTO;
+import br.edu.ufop.web.users.dto.UserDTO;
+import br.edu.ufop.web.users.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/status")
     public ResponseEntity<String> getStatus() {
         return ResponseEntity.ok("User service is running");
     }
 
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
+    }
 
-
-
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody CreateUserDTO dto) {
+        return ResponseEntity.ok( userService.save(dto) );
+    }
 
 
 
