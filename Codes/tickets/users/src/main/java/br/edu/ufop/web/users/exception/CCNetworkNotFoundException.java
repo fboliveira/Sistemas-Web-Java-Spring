ced27412652c7d4d8a40ee9901c1a8465ceb78dd
Fernando B Oliveira <fboliveira25@gmail.com>
2025-11-19 17:08:45 -0300

    added: custom ccn exception

---
+++ Codes/tickets/users/src/main/java/br/edu/ufop/web/users/exception/CCNetworkNotFoundException.java
@@ -0,0 +1,9 @@
+package br.edu.ufop.web.users.exception;
+
+public class CCNetworkNotFoundException extends RuntimeException {
+
+    public CCNetworkNotFoundException(String message) {
+        super(message);
+    }
+
+}
