1dbe0346415ed6d6df558efcc4e15b914a0983b0
Fernando B Oliveira <fboliveira25@gmail.com>
2025-11-24 22:10:39 -0300

    updated: other validations and custom exception

--- Codes/tickets/users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
+++ Codes/tickets/users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
@@ -3,11 +3,13 @@ package br.edu.ufop.web.users.domain.usecase;
 import br.edu.ufop.web.users.domain.CCNetworkDomain;
 import br.edu.ufop.web.users.domain.UserDomain;
 import br.edu.ufop.web.users.domain.port.CCNRepositoryPort;
-import lombok.AllArgsConstructor;
+import br.edu.ufop.web.users.enums.EnumUserType;
+import br.edu.ufop.web.users.exception.UseCaseException;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,36 +27,57 @@ public class CreateUserUseCase {
 
         // Demais validações
         validateCCNetworkId();
+        validateEMail();
+        validadePassword();
+        validadeUserType();
 
     }
 
     private void validateName() {
         if (this.userDomain.getName() == null) {
-            throw new RuntimeException("Name is null");
+            throw new UseCaseException("Name is null");
         }
     }
 
     private void validateCCNetworkId() {
 
         if (this.userDomain.getCcNetwork() == null) {
-            throw new RuntimeException("Invalid Credit Card Network");
+            throw new UseCaseException("Invalid Credit Card Network");
         }
 
         UUID id = this.userDomain.getCcNetwork().getId();
 
         if (id == null) {
-            throw new RuntimeException("Credit Card Network ID is null");
+            throw new UseCaseException("Credit Card Network ID is null");
         }
 
         Optional<CCNetworkDomain> domainOptional = repositoryPort.findById(id);
 
         if (domainOptional.isEmpty()) {
-            throw new RuntimeException("Credit Card Network ID does not exist.");
+            throw new UseCaseException("Credit Card Network ID does not exist.");
         }
 
         this.userDomain.setCcNetwork(domainOptional.get());
 
 
+    }
+
+    private void validateEMail() {
+        if (this.userDomain.getEmail() == null) {
+            throw new UseCaseException("E-mail is null.");
+        }
+    }
+
+    private void validadePassword() {
+        if (this.userDomain.getPassword() == null) {
+            throw new UseCaseException("Password is null");
+        }
+    }
+
+    private void validadeUserType() {
+        if (Objects.isNull(this.userDomain.getType())) {
+            this.userDomain.setType(EnumUserType.CUSTOMER);
+        }
     }
 
 
