c56fe4e1b3c3c070f5374949b49222f33df9b5ae
Fernando B Oliveira <fboliveira25@gmail.com>
2025-11-26 20:52:33 -0300

    updated: update and delete user

--- Codes/tickets/users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
+++ Codes/tickets/users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
@@ -1,6 +1,8 @@
 package br.edu.ufop.web.users.controller;
 
 import br.edu.ufop.web.users.dto.CreateUserDTO;
+import br.edu.ufop.web.users.dto.DeleteUserDTO;
+import br.edu.ufop.web.users.dto.UpdateUserDTO;
 import br.edu.ufop.web.users.dto.UserDTO;
 import br.edu.ufop.web.users.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -48,6 +50,39 @@ public class UserController {
         return ResponseEntity.ok(userDTOOptional.get());
 
 
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<UserDTO>> getByName(@PathVariable(value = "name") String name) {
+
+        List<UserDTO> userDTOList = userService.getByName(name);
+
+        return userDTOList.isEmpty() ?
+                ResponseEntity.notFound().build() :
+                ResponseEntity.ok(userDTOList);
+
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDTO> updateUser(
+            @RequestBody UpdateUserDTO updateUserDTO
+            ) {
+
+        UserDTO userDTO = userService.updateUser(updateUserDTO);
+        return ResponseEntity.ok(userDTO);
+
+    }
+
+    // Put e-mail, password (antiga e nova), número do cartão de crédito,
+    // bandeira do cartão de crédito
+
+    // Delete
+    @DeleteMapping
+    public ResponseEntity<Void> delete(
+            @RequestBody DeleteUserDTO deleteUserDTO
+            ) {
+        userService.delete(deleteUserDTO);
+        return ResponseEntity.ok().build();
     }
 
 
