42ef142dddb4458f179c1e51db8bdaf83ce08936
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 16:53:16 -0300

    added: event controller - get all and create

---
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/controller/dtos/EventController.java
@@ -0,0 +1,29 @@
+package br.edu.ufop.web.sales.controller.dtos;
+
+import br.edu.ufop.web.sales.business.services.EventService;
+import br.edu.ufop.web.sales.controller.dtos.events.CreateEventDTO;
+import br.edu.ufop.web.sales.controller.dtos.events.EventDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping
+    public ResponseEntity<List<EventDTO>> getAll() {
+        return ResponseEntity.ok(eventService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> create(@RequestBody CreateEventDTO createEventDTO) {
+        return ResponseEntity.ok(eventService.create(createEventDTO)); 
+    }
+
+}
