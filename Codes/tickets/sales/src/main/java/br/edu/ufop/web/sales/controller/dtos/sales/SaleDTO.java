1b526b5a01469ab6da94a5e3b8f035c532906aed
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 16:11:05 -0300

    added: sale DTO

---
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/controller/dtos/sales/SaleDTO.java
@@ -0,0 +1,28 @@
+package br.edu.ufop.web.sales.controller.dtos.sales;
+
+import br.edu.ufop.web.sales.controller.dtos.events.EventDTO;
+import br.edu.ufop.web.sales.enums.EnumSaleStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleDTO {
+
+    private UUID id;
+    private UUID user_id;
+
+    private EventDTO event;
+
+    private LocalDateTime dateTime;
+
+    private EnumSaleStatus status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
