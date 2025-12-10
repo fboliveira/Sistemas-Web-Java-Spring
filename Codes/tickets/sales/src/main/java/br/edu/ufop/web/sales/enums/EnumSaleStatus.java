ad22bd6cfcedcc7a0e3409e71c6a32ec7813d6dc
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 16:01:23 -0300

    added: enum - sale status

---
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/enums/EnumSaleStatus.java
@@ -0,0 +1,18 @@
+package br.edu.ufop.web.sales.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum EnumSaleStatus {
+
+    EM_ABERTO(1, "Em aberto"),
+    PAGO(2, "Pago"),
+    CANCELADO(3, "Cancelado"),
+    ESTORNADO(4, "Estornado");
+
+    private Integer id;
+    private String description;
+
+}
