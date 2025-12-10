74f66b996efe803ad63020c7242e7403abe88b98
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 16:01:12 -0300

    added: entity - event

---
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/infrastructure/entities/EventEntity.java
@@ -0,0 +1,49 @@
+package br.edu.ufop.web.sales.infrastructure.entities;
+
+import br.edu.ufop.web.sales.enums.EnumEventType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_events")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String description;
+
+    private EnumEventType type;
+
+    private LocalDateTime dateTime;
+
+    private LocalDateTime startingSales;
+    private LocalDateTime endingSales;
+
+    private Float price;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void beforeSave() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void beforeUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
