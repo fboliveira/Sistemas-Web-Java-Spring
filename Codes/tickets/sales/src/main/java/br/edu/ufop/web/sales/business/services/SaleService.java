41d42b2898644be5de1eefe0f6dc3780f0a93054
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 18:37:43 -0300

    updated: validate event

--- Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
@@ -3,18 +3,22 @@ package br.edu.ufop.web.sales.business.services;
 import br.edu.ufop.web.sales.business.converters.SaleConverter;
 import br.edu.ufop.web.sales.controller.dtos.sales.CreateSaleDTO;
 import br.edu.ufop.web.sales.controller.dtos.sales.SaleDTO;
+import br.edu.ufop.web.sales.infrastructure.entities.EventEntity;
 import br.edu.ufop.web.sales.infrastructure.entities.SaleEntity;
 import br.edu.ufop.web.sales.infrastructure.repositories.ISaleRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SaleService {
 
     private final ISaleRepository saleRepository;
+    private final EventService eventService;
 
     public List<SaleDTO> getAll() {
 
@@ -26,11 +30,21 @@ public class SaleService {
 
     }
 
+    @Transactional
     public SaleDTO create(CreateSaleDTO createSaleDTO) {
 
-        SaleEntity eventEntity = SaleConverter.toEntity(createSaleDTO);
-        eventEntity = saleRepository.save(eventEntity);
-        return SaleConverter.toDTO(eventEntity);
+        SaleEntity saleEntity = SaleConverter.toEntity(createSaleDTO);
+
+        Optional<EventEntity> eventEntityOptional = eventService.getById(createSaleDTO.getEventId());
+
+        if (eventEntityOptional.isEmpty()) {
+            throw new RuntimeException("Event does not exists.");
+        }
+
+        saleEntity.setEvent(eventEntityOptional.get());
+
+        saleRepository.save(saleEntity);
+        return SaleConverter.toDTO(saleEntity);
 
     }
 
