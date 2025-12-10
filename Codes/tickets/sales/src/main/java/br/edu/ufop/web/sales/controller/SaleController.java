cf65c25c8fd3d2c491ee0a8ee102218098c90843
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 16:59:33 -0300

    added: sale controller

---
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/controller/SaleController.java
@@ -0,0 +1,29 @@
+package br.edu.ufop.web.sales.controller;
+
+import br.edu.ufop.web.sales.business.services.SaleService;
+import br.edu.ufop.web.sales.controller.dtos.sales.CreateSaleDTO;
+import br.edu.ufop.web.sales.controller.dtos.sales.SaleDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sales")
+@RequiredArgsConstructor
+public class SaleController {
+
+    private final SaleService saleService;
+
+    @GetMapping
+    public ResponseEntity<List<SaleDTO>> getAll() {
+        return ResponseEntity.ok(saleService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<SaleDTO> create(@RequestBody CreateSaleDTO createSaleDTO) {
+        return ResponseEntity.ok(saleService.create(createSaleDTO));
+    }
+
+}
