e1f19b5ea3707a132c26c810b7d36f18099579d5
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 16:05:31 -0300

    added: sale repository

---
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/infrastructure/repositories/ISaleRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ufop.web.sales.infrastructure.repositories;
+
+import br.edu.ufop.web.sales.infrastructure.entities.SaleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ISaleRepository extends JpaRepository<SaleEntity, UUID> {
+
+}
