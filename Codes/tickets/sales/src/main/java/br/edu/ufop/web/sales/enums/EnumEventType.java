36280cb1444022037a32270f6a93d41ba542558d
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-10 16:00:45 -0300

    added: enum - event type

---
+++ Codes/tickets/sales/src/main/java/br/edu/ufop/web/sales/enums/EnumEventType.java
@@ -0,0 +1,19 @@
+package br.edu.ufop.web.sales.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum EnumEventType {
+
+    PALESTRA(1, "Palestra"),
+    SHOW(2, "Show"),
+    TEATRO(3, "Teatro"),
+    CURSO(4, "Curso"),
+    GERAL(5, "Geral/não especificado");
+
+    private Integer id;
+    private String description;
+
+}
