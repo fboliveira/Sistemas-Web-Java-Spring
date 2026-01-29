64e59c27d4550216d28e894c4f177dc59bd52b58
Fernando B Oliveira <fboliveira25@gmail.com>
2026-01-29 10:39:26 -0300

    added: list user page

---
+++ Codes/tickets/frontend/src/pages/users/ListUsers.tsx
@@ -0,0 +1,40 @@
+import api from "@/services/api"
+import type { UserInterface } from "@/types/users"
+import { useEffect, useState } from "react"
+
+
+const ListUsers = () => {
+
+    // Hook: useState
+    const [ users, setUsers ] = useState<UserInterface[]>([])
+
+    // Hook: useEffect
+    useEffect(() => {
+
+        api.get('/api/users')
+            .then(response => {
+                console.log(response)
+                setUsers(response.data)
+            })
+
+    }, [])
+
+    return(
+        <>
+            <h2>Lista de usuários</h2>
+
+            <ul>
+
+                {
+                    users.map( user => (
+                        <li key={user.id}>{user.name}</li>
+                    ) )
+                }
+
+            </ul>
+        </>
+    )
+    
+}
+
+export default ListUsers
